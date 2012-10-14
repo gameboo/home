c91190229d0bbb6a7ec4f8070317d125b56169da
Alexandre JOANNOU <alexandre.joannou@gmail.com>
2012-10-14 17:11:20 +0200

    added  181.fm radio scripts

---
+++ bin/80s-hairband.sh
@@ -0,0 +1,5 @@
+mplayer http://uplink2.181.fm:8014/
+#mplayer http://relay1.181.fm:8014/
+#mplayer http://uplink.181.fm:8014/
+#mplayer http://relay.181.fm:8014/
+#mplayer http://relay2.181.fm:8014/
